89286503f2f61b00740bf719cf27cbd429a70a41
James Atkins <hello@jamesatkins.net>
2020-10-11 13:30:47 +0200

    Gitscraping in earnest
    Remove multiple incident files. Instead use one big JSON file, sorted by incident reference.

--- ukpowernetworks.py
+++ ukpowernetworks.py
@@ -3,12 +3,17 @@ import pathlib
 import urllib.parse
 from typing import Iterator
 
-import dateutil.parser
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+DATA_DIR = pathlib.Path("data")
+RETRIES = 3
+TIMEOUT = 5
 
 
 def get_incident_ids(session: requests.Session) -> Iterator[str]:
-    r = session.get("https://www.ukpowernetworks.co.uk/Incidents/GetIncidents")
+    r = session.get("https://www.ukpowernetworks.co.uk/Incidents/GetIncidents", timeout=TIMEOUT)
     r.raise_for_status()
     data = r.json()
 
@@ -29,28 +34,39 @@ def get_incident_ids(session: requests.Session) -> Iterator[str]:
 
 
 def get_incident_details(session: requests.Session, incident_id: str) -> dict:
-    r = session.get(f"https://www.ukpowernetworks.co.uk/Incidents/getincidentdetails?incidentid={incident_id}")
+    r = session.get(f"https://www.ukpowernetworks.co.uk/Incidents/getincidentdetails?incidentid={incident_id}", timeout=TIMEOUT)
     r.raise_for_status()
     return r.json()
 
 
-ROOT_DIRECTORY = pathlib.Path("data/ukpowernetworks")
+if __name__ == "__main__":
+    retry_strategy = Retry(
+        total=RETRIES,
+        status_forcelist=[429, 500, 502, 503, 504],
+        method_whitelist=["HEAD", "GET", "OPTIONS"]
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    with requests.Session() as session:
+        session.mount("https://", adapter)
+        
+        incident_ids = list(get_incident_ids(session))
+        incident_ids.sort()
 
-with requests.Session() as session:
-    for incident_id in get_incident_ids(session):
-        print(f"Incident reference: {incident_id}")
-        incident = get_incident_details(session, incident_id)
+        incidents = {}
 
-        creation_time = dateutil.parser.parse(incident["UKPNIncident"]["CreationDateTime"])
+        for incident_id in incident_ids:
+            print(f"Incident reference: {incident_id}")
+            incident = get_incident_details(session, incident_id)
 
-        # Remove timestamp field as it seems to change regardless
-        try:
-            del incident["UKPNIncident"]["Timestamp"]
-        except KeyError:
-            pass
+            # Remove timestamp field as it seems to change regardless
+            try:
+                del incident["UKPNIncident"]["Timestamp"]
+            except KeyError:
+                pass
 
-        directory = ROOT_DIRECTORY / str(creation_time.year) / str(creation_time.month)
-        directory.mkdir(parents=True, exist_ok=True)
+            incidents[incident_id] = incident
 
-        with open(directory / f"{incident_id}.json", "w", newline="\n", encoding="utf-8") as f:
-            json.dump(incident, f, ensure_ascii=False, indent=2)
+    DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+    with open(DATA_DIR / "ukpowernetworks.json", "w", newline="\n", encoding="utf-8") as f:
+        json.dump(incidents, f, ensure_ascii=False, indent=2)
