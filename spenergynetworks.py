e96ef40fcd6d24b052747b508c4d46610cd61861
James Atkins <hello@jamesatkins.net>
2020-10-29 21:33:32 +0000

    Handle SP Energy Networks not being available

--- spenergynetworks.py
+++ spenergynetworks.py
@@ -12,13 +12,18 @@ if __name__ == "__main__":
     with make_session() as session:
         # Website is quite slow so have a high timeout threshold
         r = session.get("https://www.spenergynetworks.co.uk/pages/power_cuts_map.aspx", timeout=30)
+        r.raise_for_status()
 
-        m = re.search("arrPowercutsPostcodes: (?P<data>\[.*\]),", r.text)
-        data = json.loads(m["data"])
+        if r.url == "https://www.spenergynetworks.co.uk/pages/power_cuts_not_available.aspx":
+            # Not available
+            pass
+        else:
+            m = re.search("arrPowercutsPostcodes: (?P<data>\[.*\]),", r.text)
+            data = json.loads(m["data"])
 
-        for incident in data:
-            incident_ref = incident["INCIDENT_REF"]
-            print(f"Incident ref: {incident_ref}")
+            for incident in data:
+                incident_ref = incident["INCIDENT_REF"]
+                print(f"Incident ref: {incident_ref}")
 
-            with open(DATA_DIR / f"{incident_ref}.json", "w", newline="\n", encoding="utf-8") as f:
-                json.dump(incident, f, ensure_ascii=False, indent=2)
+                with open(DATA_DIR / f"{incident_ref}.json", "w", newline="\n", encoding="utf-8") as f:
+                    json.dump(incident, f, ensure_ascii=False, indent=2)
