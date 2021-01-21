8f0b8c1a498d07847b0c28d34243c14fb8b81343
James Atkins <hello@jamesatkins.net>
2021-01-21 09:46:42 +0000

    Fix timeout error for SPE

--- spenergynetworks.py
+++ spenergynetworks.py
@@ -3,6 +3,7 @@ import pathlib
 import re
 
 from common import make_session
+from requests import ConnectionError
 
 DATA_DIR = pathlib.Path("data") / "spenergynetworks"
 
@@ -11,20 +12,24 @@ if __name__ == "__main__":
 
     with make_session() as session:
         # Website is quite slow so have a high timeout threshold
-        r = session.get("https://www.spenergynetworks.co.uk/pages/power_cuts_map.aspx", timeout=30)
-        r.raise_for_status()
-
-        if r.url in ["https://www.spenergynetworks.co.uk/pages/power_cuts_not_available.aspx",
-                     "https://www.spenergynetworks.co.uk/pages/500.aspx"]:
-            # Not available
+        try:
+            r = session.get("https://www.spenergynetworks.co.uk/pages/power_cuts_map.aspx", timeout=30)
+        except ConnectionError:
             pass
         else:
-            m = re.search("arrPowercutsPostcodes: (?P<data>\[.*\]),", r.text)
-            data = json.loads(m["data"])
-            if data:
-                for incident in data:
-                    incident_ref = incident["INCIDENT_REF"]
-                    print(f"Incident ref: {incident_ref}")
+            r.raise_for_status()
 
-                    with open(DATA_DIR / f"{incident_ref}.json", "w", newline="\n", encoding="utf-8") as f:
-                        json.dump(incident, f, ensure_ascii=False, indent=2)
+            if r.url in ["https://www.spenergynetworks.co.uk/pages/power_cuts_not_available.aspx",
+                         "https://www.spenergynetworks.co.uk/pages/500.aspx"]:
+                # Not available
+                pass
+            else:
+                m = re.search("arrPowercutsPostcodes: (?P<data>\[.*\]),", r.text)
+                data = json.loads(m["data"])
+                if data:
+                    for incident in data:
+                        incident_ref = incident["INCIDENT_REF"]
+                        print(f"Incident ref: {incident_ref}")
+
+                        with open(DATA_DIR / f"{incident_ref}.json", "w", newline="\n", encoding="utf-8") as f:
+                            json.dump(incident, f, ensure_ascii=False, indent=2)
